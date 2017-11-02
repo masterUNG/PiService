c8ab73db0b976681110c40f1e0f33e50627d82a8
MasterUNG <phrombutr@gmail.com>
2017-11-02 16:03:20 +0700

    finish 2Nov

--- app/src/main/java/app/ewtc/masterung/piservice/fragment/RegisterFragment.java
+++ app/src/main/java/app/ewtc/masterung/piservice/fragment/RegisterFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import app.ewtc.masterung.piservice.MainActivity;
 import app.ewtc.masterung.piservice.R;
@@ -15,9 +17,13 @@ import app.ewtc.masterung.piservice.R;
  * Created by masterung on 31/10/2017 AD.
  */
 
-public class RegisterFragment extends Fragment{
+public class RegisterFragment extends Fragment {
 
-//    Create Main Method คือ เมธอดที่ ทำหน้าที่ เป็น ผู้จัดการ
+//    Explicit การประกาศตัวแปร
+    private String nameString, genderString, userString, passwordString;
+
+
+    //    Create Main Method คือ เมธอดที่ ทำหน้าที่ เป็น ผู้จัดการ
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -25,14 +31,63 @@ public class RegisterFragment extends Fragment{
 //        Create Toolbar
         createToolbar();
 
+//        Save Controller
+        saveController();
+
+
     }   // Main Method
+
+    private void saveController() {
+        ImageView imageView = getView().findViewById(R.id.imvSave);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Initial View
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+//                Get Value From EditText To String
+//                คือการรับค่าจาก ตัวแปร EditText ไปเป็น String
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+//                Check Space
+                if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+                    // Work When Condition is True ==> Have Space
+
+                }
+
+            }   // onClick
+        });
+    }
 
     private void createToolbar() {
 
 //        Initial View  คือการ Inflater View เข้ามาทำงาน ใน Java
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+
 //        Setup Toolbar คือการกำหนดให้ Toolbar ทำงาน
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title of Toolbar คือการ เปลี่ยน Title ของ Toolbar
+        ((MainActivity) getActivity())
+                .getSupportActionBar()
+                .setTitle(getResources().getString(R.string.new_register));
+
+//        Visible Button Back
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                คือการสลาย Fragment Register และ กลับไปที่หน้าที่มา
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
 
     }
 
---
+++ app/src/main/java/app/ewtc/masterung/piservice/utility/MyAlert.java
@@ -0,0 +1,41 @@
+package app.ewtc.masterung.piservice.utility;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+import app.ewtc.masterung.piservice.R;
+
+/**
+ * Created by masterung on 2/11/2017 AD.
+ */
+
+public class MyAlert {
+
+    //    Explicit
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void myDialog(String strTitle, String strMessage) {
+
+//        You Inherited Object From Class
+//        สร้าง ออฟเจ็ค ที่สามารถสร้างป้อปอัพ
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+}   // Main Class
