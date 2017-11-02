07f9d8e4e50b262bd805b221b045beb93e1398a3
MasterUNG <phrombutr@gmail.com>
2017-11-02 11:45:09 +0700

    Create Toolbar

--- app/src/main/java/app/ewtc/masterung/piservice/fragment/RegisterFragment.java
+++ app/src/main/java/app/ewtc/masterung/piservice/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package app.ewtc.masterung.piservice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import app.ewtc.masterung.piservice.MainActivity;
 import app.ewtc.masterung.piservice.R;
 
 /**
@@ -15,6 +17,27 @@ import app.ewtc.masterung.piservice.R;
 
 public class RegisterFragment extends Fragment{
 
+//    Create Main Method คือ เมธอดที่ ทำหน้าที่ เป็น ผู้จัดการ
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    }   // Main Method
+
+    private void createToolbar() {
+
+//        Initial View  คือการ Inflater View เข้ามาทำงาน ใน Java
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+//        Setup Toolbar คือการกำหนดให้ Toolbar ทำงาน
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+    }
+
+    //    เมธอด ที่ใช่ในการสร้าง หน้ากาก
+//    Create Fragment
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
